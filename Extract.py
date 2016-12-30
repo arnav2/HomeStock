18c726ead08dd714857be49e4413d46b263a60b3
arnav <arnavgoel2@gmail.com>
2016-12-30 04:11:16 -0500

    Add files via upload
    Extracted all the files

--- Extract.py
+++ Extract.py
@@ -1,6 +1,8 @@
 import urllib2
 import datetime
 import time
+import re
+import csv
 ## dd/mm/yyyy format
 day=int(time.strftime("%d"))
 year=time.strftime("%Y")
@@ -17,7 +19,7 @@ tu=(day-1,month,year,monthinteger)
 url =('https://www.nseindia.com/content/historical/EQUITIES/{2}/{1}/cm{0}{1}{2}bhav.csv.zip'.format(*tu))
 url2=('https://www.nseindia.com/content/historical/DERIVATIVES/{2}/{1}/fo{0}{1}{2}bhav.csv.zip'.format(*tu))
 url3=('https://www.nseindia.com/archives/nsccl/mwpl/combineoi_{0}{3}{2}.zip'.format(*tu))
-
+url4=('https://www.nseindia.com/archives/equities/mto/MTO_{0}{3}{2}.DAT'.format(*tu))
 hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
        'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
@@ -45,3 +47,15 @@ f3 = urllib2.urlopen(req3)
 data3 = f3.read()
 with open("test7.zip", "wb") as code:
     code.write(data3)
+
+req = urllib2.Request(url4,headers=hdr)
+resp = urllib2.urlopen(req)
+respData = resp.read()
+print respData
+
+file = open("newfile.txt", "w")
+file.write(respData)
+file.close()
+#with open('some.csv', 'wb') as f:
+#    writer = csv.writer(f)
+#    writer.writerows(respData)
